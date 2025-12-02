35bb3e10fce274ef14162703be502c7528935edb
Ali Ghahremani <alighahremani1377@gmail.com>
2025-12-02 13:46:09 +0330

    feat: worm home

--- core/src/utils/mod.rs
+++ core/src/utils/mod.rs
@@ -1,5 +1,6 @@
 pub mod poseidon;
 pub mod u256_fr_conversion;
+pub mod worm_home;
 
 pub use poseidon::*;
 pub use u256_fr_conversion::*;
---
+++ core/src/utils/worm_home.rs
@@ -0,0 +1,24 @@
+use std::path::PathBuf;
+
+use anyhow::anyhow;
+
+pub fn get_worm_home() -> Result<PathBuf, anyhow::Error> {
+    let home = std::env::home_dir().ok_or(anyhow!("no home directory"))?;
+    Ok(home.join(".worm-miner"))
+}
+
+pub fn get_proof_of_burn_dat() -> Result<PathBuf, anyhow::Error> {
+    Ok(get_worm_home()?.join("proof_of_burn.dat"))
+}
+
+pub fn get_proof_of_burn_zkey() -> Result<PathBuf, anyhow::Error> {
+    Ok(get_worm_home()?.join("proof_of_burn.zkey"))
+}
+
+pub fn get_spend_dat() -> Result<PathBuf, anyhow::Error> {
+    Ok(get_worm_home()?.join("spend.dat"))
+}
+
+pub fn get_spend_zkey() -> Result<PathBuf, anyhow::Error> {
+    Ok(get_worm_home()?.join("spend.zkey"))
+}
