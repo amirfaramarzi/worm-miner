5154b773876dfd386c118cf11fb7beb2f92db5fe
Ali Ghahremani <alighahremani1377@gmail.com>
2025-12-22 16:52:02 +0330

    feat: get min prover fee and min broadcaster fee from server

--- server/src/handlers/proof_get.rs
+++ server/src/handlers/proof_get.rs
@@ -1,18 +1,36 @@
-use crate::{data::AppState, error::ServerError};
-use axum::{extract::State, response::IntoResponse};
+use crate::{
+    data::AppState,
+    error::{LogIfError, ServerError},
+};
+use alloy::primitives::U256;
+use anyhow::anyhow;
+use axum::{Json, extract::State, response::IntoResponse};
+use common::utils::ether_amount_serializer;
+use serde::Serialize;
 use std::sync::{Arc, RwLock};
 
 /// GET `/proof` returns minimum proving fee of the relayer.
 pub async fn proof_get(
     State(state): State<Arc<RwLock<AppState>>>,
 ) -> Result<ProofGetResponse, ServerError> {
-    todo!();
+    let min_prover_fee = state
+        .read()
+        .map_err(|e| ServerError::Unexpected(anyhow!("{e}")))
+        .log_with_context("get_min_prover_fee")?
+        .config
+        .min_prover_fee;
+
+    Ok(ProofGetResponse { min_prover_fee })
 }
 
-pub struct ProofGetResponse {}
+#[derive(Serialize)]
+pub struct ProofGetResponse {
+    #[serde(with = "ether_amount_serializer")]
+    min_prover_fee: U256,
+}
 
 impl IntoResponse for ProofGetResponse {
     fn into_response(self) -> axum::response::Response {
-        todo!()
+        Json(&self).into_response()
     }
 }
--- server/src/handlers/relay_get.rs
+++ server/src/handlers/relay_get.rs
@@ -1,18 +1,38 @@
-use crate::{data::AppState, error::ServerError};
-use axum::{extract::State, response::IntoResponse};
+use crate::{
+    data::AppState,
+    error::{LogIfError, ServerError},
+};
+use alloy::primitives::U256;
+use anyhow::anyhow;
+use axum::{Json, extract::State, response::IntoResponse};
+use common::utils::ether_amount_serializer;
+use serde::Serialize;
 use std::sync::{Arc, RwLock};
 
 /// GET `/relay` returns minimum broadcasting fee of the relayer
 pub async fn relay_get(
     State(state): State<Arc<RwLock<AppState>>>,
 ) -> Result<RelayGetResponse, ServerError> {
-    todo!();
+    let min_broadcaster_fee = state
+        .read()
+        .map_err(|e| ServerError::Unexpected(anyhow!("{e}")))
+        .log_with_context("get_min_broadcaster_fee")?
+        .config
+        .min_broadcaster_fee;
+
+    Ok(RelayGetResponse {
+        min_broadcaster_fee,
+    })
 }
 
-pub struct RelayGetResponse {}
+#[derive(Serialize)]
+pub struct RelayGetResponse {
+    #[serde(with = "ether_amount_serializer")]
+    min_broadcaster_fee: U256,
+}
 
 impl IntoResponse for RelayGetResponse {
     fn into_response(self) -> axum::response::Response {
-        todo!()
+        Json(self).into_response()
     }
 }
