7391dbcddd75b2dd60e9551f09625e0b780c305c
Ali Ghahremani <alighahremani1377@gmail.com>
2025-12-24 12:15:53 +0330

    ref: witness file only needs header and not whole block

--- common/src/mint/witness_input_file.rs
+++ common/src/mint/witness_input_file.rs
@@ -2,10 +2,10 @@ use std::{fs, path::PathBuf};
 
 use crate::consts::BURN_AMOUNT_LIMIT;
 use alloy::{
+    consensus::Header,
     primitives::{Address, U256, keccak256},
-    rlp::Encodable,
-    rlp::RlpDecodable,
-    rpc::types::{Block, EIP1186AccountProofResponse},
+    rlp::{Encodable, RlpDecodable},
+    rpc::types::EIP1186AccountProofResponse,
 };
 use alloy_rlp::Decodable;
 use anyhow::anyhow;
@@ -41,18 +41,18 @@ pub struct WitnessInputFile {
 impl WitnessInputFile {
     pub fn new(
         proof: EIP1186AccountProofResponse,
-        block: Block,
+        block_header: Header,
         burn_key: Fr,
         spend: U256,
         burn_extra_commitment: Fr,
         prover: Address,
     ) -> Result<Self, WitnessInputFileError> {
-        if !block.header.state_root == keccak256(&proof.account_proof[0]) {
+        if !block_header.state_root == keccak256(&proof.account_proof[0]) {
             return Err(WitnessInputFileError::NotOnSameBlock);
         }
 
         let mut header_bytes = vec![];
-        block.header.inner.encode(&mut header_bytes);
+        block_header.encode(&mut header_bytes);
 
         let leaf = proof
             .account_proof
