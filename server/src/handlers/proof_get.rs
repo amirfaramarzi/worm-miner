4e5659ee39be440fb4bd54cc4f1483646d6014b3
Ali Ghahremani <alighahremani1377@gmail.com>
2026-01-25 16:55:11 +0330

    feat: prover address added to proof_get end-point response

--- server/src/handlers/proof_get.rs
+++ server/src/handlers/proof_get.rs
@@ -1,5 +1,5 @@
 use crate::{data::AppState, error::ServerError};
-use alloy::primitives::U256;
+use alloy::primitives::{Address, U256};
 use axum::{Json, extract::State, response::IntoResponse};
 use common::utils::ether_amount_serializer;
 use serde::Serialize;
@@ -14,6 +14,7 @@ pub async fn proof_get(
 
     Ok(ProofGetResponse {
         min_prover_fee: state.config.min_prover_fee,
+        prover_address: state.config.address(),
     })
 }
 
@@ -21,6 +22,7 @@ pub async fn proof_get(
 pub struct ProofGetResponse {
     #[serde(with = "ether_amount_serializer")]
     min_prover_fee: U256,
+    prover_address: Address,
 }
 
 impl IntoResponse for ProofGetResponse {
