68b5ee9a2ef268986ceaecd87d9c647cf297eca6
Keyvan Kambakhsh <keyvankambakhsh@gmail.com>
2026-01-27 15:59:30 +0330

    feat: Do not allow duplicate jobs

--- server/src/handlers/proof_post.rs
+++ server/src/handlers/proof_post.rs
@@ -50,6 +50,24 @@ pub async fn proof_post(
     let job_id = state.next_job_id;
     let job = ProofJob::new(job_id, body, block_header);
     let nullifier = job.nullifier;
+
+    if let Some(user_job_id) = state.nullifier_to_job_id.get(&nullifier) {
+        if state.proof_cache.contains_key(&nullifier) {
+            // Proof is already created!
+            return Ok(ProofPostResponse {});
+        } else {
+            if let Some(running_job_id) = state.current_processing_job_id {
+                if running_job_id <= *user_job_id {
+                    // Proof is already in queue!
+                    return Ok(ProofPostResponse {});
+                } else {
+                    // Loop is processing a newer job id but proof doesn't exist in cache!
+                    state.nullifier_to_job_id.remove(&nullifier);
+                }
+            }
+        }
+    }
+
     if let Err(e) = state.job_channel.send(job) {
         return Err(ServerError::Unexpected(
             anyhow!("{e}").into_boxed_dyn_error(),
